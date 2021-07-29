f263f5a65f691ebd76413a6c6aba72506ad7e4f0
gooosedev <gooosedev@DESKTOP-0JF270M>
2021-07-29 21:24:43 +0300

    Add Integration Tests.

---
+++ Tourism.Server.IntegrationTest/ForumControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Tourism.Server.Data;
+using Tourism.Server.IntegrationTests.Models;
+using Xunit;
+
+namespace Tourism.Server.IntegrationTests
+{
+    public class ForumControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public ForumControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task GetAll_Returns200()
+        {
+            var response = await _client.GetAsync("/api/forum");
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsMediaType()
+        {
+            var response = await _client.GetAsync("/api/forum");
+
+            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsContent()
+        {
+            var response = await _client.GetAsync("/api/forum");
+
+            Assert.NotNull(response.Content);
+            Assert.True(response.Content.Headers.ContentLength > 0);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsExpectedJson()
+        {
+            var responseStream = await _client.GetStreamAsync("/api/forum");
+            var model = await JsonSerializer.DeserializeAsync<ExpectedForumsModel>(responseStream,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ;
+
+            Assert.NotNull(model?.items);
+        }
+    }
+}
---
+++ Tourism.Server.IntegrationTest/Models/ExpectedForumsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tourism.Server.Models;
+
+namespace Tourism.Server.IntegrationTests.Models
+{
+    public class ExpectedForumsModel {
+        public IEnumerable<Forum> items { get; set; }
+
+    }
+}
