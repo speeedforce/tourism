7debc5e1a5aba94de08d110f7a008a3a9dda5c6e
gooosedev <gooosedev@DESKTOP-0JF270M>
2021-08-05 15:53:48 +0300

    Add ViewModels and create new ui components.

--- Tourism/Shared/ViewModels/Forum/ForumCreateViewMode.cs
+++ Tourism/Shared/ViewModels/Forum/ForumCreateViewMode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,13 @@ namespace Tourism.Shared.ViewModels.Forum
 {
     public class ForumCreateViewMode
     {
+
+        [Required]
+        [StringLength(32, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 8)]
         public string Title { get; set; }
+
+        [Required]
+        [StringLength(240, ErrorMessage = "{0} length must be between {2} and {1}.", MinimumLength = 32)]
         public string Description { get; set; }
 
         public DateTime Created { get; set; }
