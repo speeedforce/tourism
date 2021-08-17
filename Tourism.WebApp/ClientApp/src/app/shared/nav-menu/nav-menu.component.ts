f668099cfac10b42ede1436b3a1b3540aaa1ce95
gooosedev <gooosedev@DESKTOP-0JF270M>
2021-08-17 17:54:50 +0300

    remove reduntant files

--- Tourism.WebApp/ClientApp/src/app/shared/nav-menu/nav-menu.component.ts
+++ Tourism.WebApp/ClientApp/src/app/shared/nav-menu/nav-menu.component.ts
@@ -1,3 +1,4 @@
+import { generateMenu, generateMenuRegister, ILink } from './menu-config';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/api-authorization/authorize.service';
@@ -9,6 +10,21 @@ import { AuthenticationService } from 'src/api-authorization/authorize.service';
 })
 export class NavMenuComponent {
   isExpanded = false;
+  menuItems: ILink[] = generateMenu();
+
+  public href: string = "/";
+
+  constructor(private router: Router, private authService: AuthenticationService) {
+    this.href = this.router.url;
+
+    this.authService.user.subscribe(user => {
+      if (user !== null && user !== undefined) {
+        this.menuItems = generateMenuRegister(this.authService.userValue.username);
+      } else {
+        this.menuItems = generateMenu();
+      }
+    })
+  }
 
   collapse() {
     this.isExpanded = false;
