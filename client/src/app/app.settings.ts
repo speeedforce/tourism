cb5ab0a191d4c39fc55c8dcf0206e6a485d8fb29
gooosedev <gooosedev@DESKTOP-0JF270M>
2021-09-30 16:32:21 +0300

    update api

--- client/src/app/app.settings.ts
+++ client/src/app/app.settings.ts
@@ -1,7 +1,7 @@
 export class AppSettings {
     
     static get API_URL(): string {
-        const path = `http://${window.location.hostname}/toursim-api/api`;
+        const path = `http://${window.location.hostname}/tourism-api/api`;
         return path;
     }
 
